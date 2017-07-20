244d7345c5031c0504c22978119895690299e8b8
davidtom <davidjtomczyk@gmail.com>
2017-07-20 15:59:00 -0400

    dodger can move left and right

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much."
+
+const dodger = document.getElementById("dodger")
+
+function moveDodgerLeft(){
+  let leftNumbers = dodger.style.left.replace("px", "")
+  let left = parseInt(leftNumbers, 10)
+
+  if (left > 0){
+    dodger.style.left = `${left-10}px`
+  }
+}
+
+function moveDodgerRight(){
+  let leftNumbers = dodger.style.left.replace("px", "")
+  let left = parseInt(leftNumbers, 10)
+
+  if (left<360){
+    dodger.style.left = `${left+10}px`
+  }
+}
+
+document.addEventListener("keydown", function(event){
+  if (event.which == 37){
+    moveDodgerLeft()
+  } else if (event.which == 39){
+    moveDodgerRight()
+  }
+})
